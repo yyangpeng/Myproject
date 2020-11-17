a9796cfb6c940581a968874e78c628d68102b495
yangyueyue <yangyueyueaa@163.com>
2020-11-17 23:46:00 +0800

    yangyueyue创建了分支member

---
+++ member/src/main/java/com/yang/member/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.yang.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/17 - 23:43
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
