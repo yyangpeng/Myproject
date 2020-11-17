13d67012295f32348631b8bbc3b310cc578ba0e1
yangyueyue <yangyueyueaa@163.com>
2020-11-18 00:07:51 +0800

    yangyueyue新建分支search

---
+++ search/src/main/java/com/yang/search/SearchApplication.java
@@ -0,0 +1,15 @@
+package com.yang.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/18 - 0:06
+ */
+@SpringBootApplication
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
