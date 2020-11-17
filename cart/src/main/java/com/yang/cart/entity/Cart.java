99362c1e0f8799779733945308dac0bb9f759191
yangyueyue <yangyueyueaa@163.com>
2020-11-17 23:00:44 +0800

    cart服务编写1

---
+++ cart/src/main/java/com/yang/cart/entity/Cart.java
@@ -0,0 +1,10 @@
+package com.yang.cart.entity;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/17 - 22:58
+ */
+public class Cart {
+    private String name;
+    private Integer age;
+}
---
+++ cart/src/main/java/com/yang/cart/mapper/CartService.java
@@ -0,0 +1,8 @@
+package com.yang.cart.mapper;
+
+/**
+ * @author yangyueyue
+ * @date 2020/11/17 - 23:00
+ */
+public class CartService {
+}
